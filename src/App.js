efd98ad5bb65a582e4c11148675ebdf4604d7361
Beau Elliott <belliott1516@gmail.com>
2022-04-26 15:50:55 -0700

    finished footer rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header.js';
+import Footer from './Components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header greeting= "Welcome to Sentient Animal Petting Zoo"/>
+        <Footer email="Alchemy@alchemy.com" />
       </header>
     </div>
   );
