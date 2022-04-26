9c4e88ea3d85331f6bc40b67b6cfa443b8348ae7
Beau Elliott <belliott1516@gmail.com>
2022-04-26 16:25:20 -0700

    got image to show up when there is something in the main section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './Components/Header/Header.js';
+import Main from './Components/Main/Main.js';
 import Footer from './Components/Footer/Footer.js';
+import { animals } from './data.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header greeting= "Welcome to Sentient Animal Petting Zoo"/>
+        <Main animals={ animals }/>
         <Footer email="Alchemy@alchemy.com" />
       </header>
     </div>
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import farmBackground from '../../background.png';
+import './main.css';
+
+export default function Main(){
+  return (
+    <div className='main' style={{ background: `url(${farmBackground})` }}>
+    </div>
+  );
+}
