3c84e2e5ede2b100d6fd9720a86beab35b11f7e7
Beau Elliott <belliott1516@gmail.com>
2022-04-26 15:42:28 -0700

    Header is connected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-  
+        <Header greeting= "Welcome to Sentient Animal Petting Zoo"/>
       </header>
     </div>
   );
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function Header({ greeting }){
+  return (
+    <div>{greeting}</div>
+  );
+}
